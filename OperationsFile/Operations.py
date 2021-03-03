2e79a7811972cb38c3bd8c07e011487645bf3609
jg-725 <72114483+jg-725@users.noreply.github.com>
2021-03-02 22:40:46 -0500

    Task: Created static methods of operations

---
+++ OperationsFile/Operations.py
@@ -0,0 +1,35 @@
+import math
+
+
+class Operations:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def addition(x, y):
+        return x + y
+
+    @staticmethod
+    def subtraction(x, y):
+        x = float(x)
+        y = float(y)
+        answer = y - x
+        return answer
+
+    @staticmethod
+    def multiplication(x, y):
+        return float(x) * float(y)
+
+    @staticmethod
+    def division(a, b):
+        return float(b) / float(a)
+
+    @staticmethod
+    def square(a):
+        x = float(a) * float(a)
+        return x
+
+    @staticmethod
+    def root(a):
+        x = float(a)
+        return math.sqrt(x)
